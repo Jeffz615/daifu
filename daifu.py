95c7ed7fc9d5fbdb941f146a1e6aa813f1f310da
Jeffz615 <5361021+jeffz615@user.noreply.gitee.com>
2020-04-28 16:03:37 +0800

    新算法，更短的密文

--- daifu.py
+++ daifu.py
@@ -1,45 +1,48 @@
 # -*- coding:utf-8 -*-
 
 daifu = '歪比巴卜'
-decode_table = dict((daifu[i], 2**(3-i)) for i in range(4))
+decode_table = dict((daifu[i], i) for i in range(4))
+print(decode_table)
 encode_table = dict((val, key) for key, val in decode_table.items())
+print(encode_table)
 
 
-def hextodaifu(hexcode: int):
-    temp = ''
-    for val in encode_table:
-        if hexcode & val:
-            temp += encode_table[val]
-            hexcode ^= val
-    return temp
+def daifu_encode(plain: bytes, encoding: str = 'utf-8') -> bytes:
+    cipher = ''
+    # print(list(plain))
+    for twohex in plain:
+        halfhex = twohex >> 6
+        cipher += encode_table[halfhex]
+        halfhex = twohex >> 4 & 3
+        cipher += encode_table[halfhex]
+        halfhex = twohex >> 2 & 3
+        cipher += encode_table[halfhex]
+        halfhex = twohex & 3
+        cipher += encode_table[halfhex]
+    return cipher.encode(encoding)
 
 
-def daifu_encode(plain: str):
-    plain_bytes = plain.encode('utf-8')
-    code_list = ['歪比歪比']
-    for twohex in plain_bytes:
-        onehex1 = twohex//16
-        onehex2 = twohex % 16
-        code_list.append(hextodaifu(onehex1))
-        code_list.append(hextodaifu(onehex2))
-    code_list.append('歪比巴卜')
-    return ' '.join(code_list)
-
-
-def daifu_decode(code: str):
-    code_list = code.split(' ')
-    if not (len(code_list) >= 2 and code_list.pop(0) == '歪比歪比' and code_list.pop() == '歪比巴卜'):
-        return ''
-    hex_text = ''
-    for code in code_list:
+def daifu_decode(cipher: bytes, encoding: str = 'utf-8') -> bytes:
+    try:
+        cipher_text = cipher.decode(encoding)
+    except:
+        raise Exception("Wrong cipher!")
+    length = len(cipher_text)
+    if length % 4:
+        raise Exception("Wrong cipher!")
+    plain_hex_list = []
+    for i in range(length//4):
         temp = 0
-        for i in code:
-            temp += decode_table[i]
-        hex_text += hex(temp)[-1]
-    return bytes.fromhex(hex_text).decode('utf-8')
+        for j in range(4):
+            temp <<= 2
+            temp += decode_table[cipher_text[i*4+j]]
+        plain_hex_list.append(temp)
+    # print(plain_hex_list)
+    return bytes(plain_hex_list)
 
 
 if __name__ == "__main__":
-    daifu_text = daifu_encode("你好")
-    print(daifu_text)
-    print(daifu_decode(daifu_text))
+    plain = "你好"
+    cipher = daifu_encode(plain.encode('utf-8')).decode('utf-8')
+    print(cipher)
+    print(daifu_decode(cipher.encode('utf-8')).decode('utf-8'))
